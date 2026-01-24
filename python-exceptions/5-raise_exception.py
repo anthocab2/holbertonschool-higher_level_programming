09e05619382e467974c06eabbdf08e3d5fd0565d
anthocab2 <anthonycaban1010@gmail.com>
2026-01-24 15:36:34 -0400

    5. Raise a TypeError exception

---
+++ python-exceptions/5-raise_exception.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+5-raise_exception.py
+Module to raise a TypeError exception.
+"""
+
+
+def raise_exception():
+    """
+    Raises a TypeError exception.
+    """
+    raise TypeError
