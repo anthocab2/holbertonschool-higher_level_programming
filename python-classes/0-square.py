1fc8765ff5930674015f9dccba9504249e1fb5d5
anthocab2 <anthonycaban1010@gmail.com>
2026-01-28 21:59:11 -0400

    Create an empty class Square with docstrings

---
+++ python-classes/0-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+This module defines an empty class Square.
+"""
+
+
+class Square:
+    """
+    A class that represents a square.
+    
+    Attributes:
+        None
+    """
+    pass
