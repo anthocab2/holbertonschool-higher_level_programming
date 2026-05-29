568a27c41f5b9bdf9ddcae4b2f43d0ed523fe80a
anthocab2 <anthonycaban1010@gmail.com>
2026-05-29 19:18:59 -0400

    Add Roman numeral to integer converter

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if not isinstance(roman_string, str):
+        return 0
+
+    roman_values = {
+        "I": 1,
+        "V": 5,
+        "X": 10,
+        "L": 50,
+        "C": 100,
+        "D": 500,
+        "M": 1000
+    }
+
+    total = 0
+    previous = 0
+
+    for char in reversed(roman_string):
+        value = roman_values[char]
+
+        if value < previous:
+            total -= value
+        else:
+            total += value
+            previous = value
+
+    return total
