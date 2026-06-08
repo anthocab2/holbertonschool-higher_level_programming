ed2dab0084834fc23b669673f963b9b2a0feedc8
anthocab2 <anthonycaban1010@gmail.com>
2026-06-07 23:07:18 -0400

    Add function to write text to a file

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Defines a function that writes a string to a UTF-8 text file."""
+
+
+def write_file(filename="", text=""):
+    """Write a string to a file and return the number of characters."""
+    with open(filename, "w", encoding="utf-8") as f:
+        return f.write(text)
