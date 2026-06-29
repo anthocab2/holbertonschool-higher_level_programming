f590352527585006ffbb4267ea333216e4f41aa6
anthocab2 <anthonycaban1010@gmail.com>
2026-06-28 21:54:59 -0400

    Add SQL script to list user privileges

---
+++ SQL_more_queries/0-privileges.sql
@@ -0,0 +1,5 @@
+-- Lists all privileges of user_0d_1 and user_0d_2.
+-- Shows grants for user_0d_1 on localhost.
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+-- Shows grants for user_0d_2 on localhost.
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
