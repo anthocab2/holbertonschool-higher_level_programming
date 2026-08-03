5a47005a630b9d0fcd941286b6fe318ffcacf666
anthocab2 <anthonycaban1010@gmail.com>
2026-08-03 09:41:19 -0400

    Add SQLAlchemy state city relationship

---
+++ python-object_relational_mapping/100-relationship_states_cities.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Creates State California with City San Francisco"""
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from relationship_state import State, Base
+from relationship_city import City
+
+
+if __name__ == "__main__":
+    engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+            sys.argv[1],
+            sys.argv[2],
+            sys.argv[3]
+        ),
+        pool_pre_ping=True
+    )
+
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    california = State(name="California")
+    california.cities.append(City(name="San Francisco"))
+
+    session.add(california)
+    session.commit()
+    session.close()
---
+++ python-object_relational_mapping/relationship_city.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Relationship City model"""
+from sqlalchemy import Column, Integer, String, ForeignKey
+from model_state import Base
+from sqlalchemy.orm import relationship
+
+
+class City(Base):
+    """Represents a city"""
+    __tablename__ = 'cities'
+
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
+
+    state = relationship("State", back_populates="cities")
